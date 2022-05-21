076fa9ab4a6f087d8b84644d61d5360835cfbf58
Marcin Chrost <marcinc@backbase.com>
2022-05-21 22:57:53 +0200

    more example for sealed interfaces

---
+++ src/main/java/com/chrosciu/sealedclasses/interfaces/lib/AnotherNameable.java
@@ -0,0 +1,8 @@
+package com.chrosciu.sealedclasses.interfaces.lib;
+
+public final class AnotherNameable /* implements Nameable */ {
+    //@Override
+    public String getName() {
+        return null;
+    }
+}
