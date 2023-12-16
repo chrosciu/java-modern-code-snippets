b8b69fdafa4405c215049400253f3cf71416e237
Marcin Chrost <mchrost82@gmail.com>
2023-12-16 07:54:55 +0100

    String templates examples

---
+++ src/main/java/eu/chrost/stringtemplates/S02FMTTemplate.java
@@ -0,0 +1,26 @@
+package eu.chrost.stringtemplates;
+
+import static java.util.FormatProcessor.FMT;
+
+class S02FMTTemplate {
+    public static void main(String[] args) {
+        record Rectangle(String name, double width, double height) {
+            double area() {
+                return width * height;
+            }
+        }
+        Rectangle[] zone = new Rectangle[]{
+                new Rectangle("Alfa", 17.8, 31.4),
+                new Rectangle("Bravo", 9.6, 12.4),
+                new Rectangle("Charlie", 7.1, 11.23),
+        };
+        String table = FMT."""
+            Description     Width    Height     Area
+            %-12s\{zone[0].name}  %7.2f\{zone[0].width}  %7.2f\{zone[0].height}     %7.2f\{zone[0].area()}
+            %-12s\{zone[1].name}  %7.2f\{zone[1].width}  %7.2f\{zone[1].height}     %7.2f\{zone[1].area()}
+            %-12s\{zone[2].name}  %7.2f\{zone[2].width}  %7.2f\{zone[2].height}     %7.2f\{zone[2].area()}
+            \{" ".repeat(28)} Total %7.2f\{zone[0].area() + zone[1].area() + zone[2].area()}
+            """;
+        System.out.println(table);
+    }
+}
