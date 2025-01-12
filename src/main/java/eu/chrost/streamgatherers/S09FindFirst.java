4092f83c1cef0bae289bbcd3f14050ac526f9ad8
Marcin Chrost <mchrost82@gmail.com>
2025-01-12 14:44:13 +0100

    map not null & find first gatherers (not implemented yet)

---
+++ src/main/java/eu/chrost/streamgatherers/S09FindFirst.java
@@ -0,0 +1,27 @@
+package eu.chrost.streamgatherers;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Gatherer;
+
+@RequiredArgsConstructor
+class FindFirstGatherer<T> implements Gatherer<T, Void, T> {
+    private final Predicate<T> predicate;
+
+    @Override
+    public Integrator<Void, T, T> integrator() {
+        return null;
+    }
+}
+
+class S09FindFirst {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 4, 5);
+        var firstEvenNumber = numbers.stream()
+                .gather(new FindFirstGatherer<>(i -> i % 2 == 0))
+                .toList();
+        System.out.println(firstEvenNumber);
+    }
+}
