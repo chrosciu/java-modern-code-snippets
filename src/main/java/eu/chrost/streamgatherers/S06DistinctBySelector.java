50e985dca4a5ec9f535bca70c6f24150c6b84398
Marcin Chrost <mchrost82@gmail.com>
2025-01-12 14:43:34 +0100

    reordered classes in DistinctByGatherer example

--- src/main/java/eu/chrost/streamgatherers/S06DistinctBySelector.java
+++ src/main/java/eu/chrost/streamgatherers/S06DistinctBySelector.java
@@ -9,14 +9,6 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Gatherer;
 
-enum Color {
-    RED,
-    BLACK,
-    WHITE;
-}
-
-record Car(String model, Color color) {}
-
 @RequiredArgsConstructor
 class DistinctByGatherer<T, P> implements Gatherer<T, Set<P>, T> {
     private final Function<T, P> selector;
@@ -32,12 +24,20 @@ class DistinctByGatherer<T, P> implements Gatherer<T, Set<P>, T> {
             P extracted = selector.apply(item);
             if(!state.contains(extracted)) {
                 state.add(extracted);
-                downstream.push(item);
+                return downstream.push(item);
             }
             return true;
         });
     }
 }
+
+enum Color {
+    RED,
+    BLACK,
+    WHITE;
+}
+
+record Car(String model, Color color) {}
 
 class S06DistinctBySelector {
     public static void main(String[] args) {
