8f82e71bc78f65f9fc3f691cb884b0b2e6ecb759
Marcin Chrost <mchrost82@gmail.com>
2025-01-12 14:43:08 +0100

    window sliding / window fixed - added average value in window calculation

--- src/main/java/eu/chrost/streamgatherers/S04WindowFixed.java
+++ src/main/java/eu/chrost/streamgatherers/S04WindowFixed.java
@@ -6,7 +6,8 @@ import java.util.stream.Stream;
 class S04WindowFixed {
     public static void main(String[] args) {
         Stream.of(1, 2, 3, 4, 5)
-                .gather(Gatherers.windowFixed(3))
+                .gather(Gatherers.windowFixed(2))
+                .map(integers -> integers.stream().mapToInt(i -> i).average().orElse(0))
                 .forEach(System.out::println);
     }
 }
